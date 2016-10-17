f2f59a85e74ef49d19f45de3f762f8e7c67ea8fe
Claire Sial and Devin Ludwig <claire+devin@>
2016-10-17 15:27:53 -0700

    add cipher project with only javascript functions

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var sentence = prompt ("Write any sentence:")
+console.info(sentence);
+
+var firstLastCap = function(sentenceParam) {
+  return sentenceParam.charAt(0).toUpperCase() + sentenceParam.charAt(sentenceParam.length-1).toUpperCase();
+};
+
+console.info(firstLastCap(sentence));
+
+var reverser = function(sentenceParam2) {
+  return sentenceParam2.charAt(sentenceParam2.length-1).toUpperCase() + sentenceParam2.charAt(0).toUpperCase();
+};
+console.info(reverser(sentence));
+
+var doubleFunction = function(sentenceParam3) {
+  return sentenceParam3 + reverser(firstLastCap(sentenceParam3))
+};
+
+console.info(doubleFunction(sentence));
+
+var count = function(sentenceParam4) {
+  return sentenceParam4.charAt(Math.floor(sentenceParam4.length / 2)) + doubleFunction(sentenceParam4)
+};
+
+console.info(count(sentence));
+
+function reverse(sentenceParam5) {
+  return sentenceParam5.split('').reverse().join('');
+}
+
+console.info(reverse(count(sentence)));
